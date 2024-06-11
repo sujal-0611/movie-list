00fccbb0a2402a15e90f9cae23e449e5ba39a5ab
sharma.sujal0611 <sharma.sujal0611@gmail.com>
2024-06-11 17:16:42 +0000

    added footer

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 // app/layout.js
 import './globals.css'
 
@@ -10,10 +9,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="min-h-screen flex flex-col">
+          <main className="flex-grow">
+            {children}
+          </main>
+          <footer className="bg-gray-800 text-white text-center p-4">
+            © Sujal Sharma. All Rights Reserved
+          </footer>
+        </div>
+      </body>
     </html>
   )
 }
-
-
-
